1ff328e108722db29273d21f00642c015bb810b1
CORR1T <corsin@ragett.li>
2022-05-09 16:53:38 +0200

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+
+
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
+    write!(vga_buffer::WRITER.lock(), "Hello again! This is number {}", 5).unwrap();
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
